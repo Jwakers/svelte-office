0f8e7849709337c683086fab449caa36cf4b318a
Jack Wakeham <jackwakeham@gmail.com>
2023-08-26 18:42:52 +0100

    Updated sort key value

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -5,7 +5,7 @@ export default function ContactPage() {
     <section className="mx-auto h-full w-full border-black p-3 md:max-w-3xl md:border-l md:border-r">
       <h1 className="font-serif text-3xl">Contact us</h1>
       <p className="mb-4 max-w-lg">
-        Please fill out this short form and we will get back to you as soon as we can
+        Please fill out this short form and we will get back to you as soon as we can.
       </p>
       <ContactForm />
     </section>
