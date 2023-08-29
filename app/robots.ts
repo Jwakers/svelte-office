855196367d09d159fafd04159e002459ebd837dc
Jack Wakeham <jackwakeham@gmail.com>
2023-08-29 20:38:18 +0100

    Updated robots with correct URL

--- app/robots.ts
+++ app/robots.ts
@@ -1,6 +1,7 @@
-const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
-  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-  : 'http://localhost:3000';
+const baseUrl =
+  process.env.NODE_ENV === 'production'
+    ? `https://${process.env.NEXT_PUBLIC_SITE_URL}`
+    : 'http://localhost:3000';
 
 export default function robots() {
   return {
