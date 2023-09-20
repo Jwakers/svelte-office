bd45e84fa2b456f8e9e503b5100e3187fd94d589
Jack Wakeham <jackwakeham@gmail.com>
2023-09-20 16:47:33 +0100

    Prevented empty collections showing

--- app/categories/page.tsx
+++ app/categories/page.tsx
@@ -23,34 +23,36 @@ export default async function Collections() {
       <h1 className="p-3 font-serif text-2xl md:text-3xl">Categories</h1>
 
       <div className="grid sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
-        {collections.map((collection) => (
-          <Link
-            href={`/categories/${collection.handle}`}
-            key={collection.handle}
-            className="group flex h-full flex-col outline outline-1 outline-black"
-          >
-            {collection.image ? (
-              <Image
-                src={collection.image.url}
-                height={320}
-                width={320}
-                alt={collection.image.altText}
-                className="aspect-square w-full object-cover"
-              />
-            ) : null}
-            <div className="flex h-full flex-col border-t border-black bg-white p-3">
-              <h2 className="mb-2 font-serif text-lg uppercase md:text-xl">{collection.title}</h2>
-              <div className="mt-auto flex justify-between">
-                <IndexString
-                  value={collection.products.length}
-                  text="products in
-                  this category"
+        {collections.map((collection) =>
+          collection.products.length ? (
+            <Link
+              href={`/categories/${collection.handle}`}
+              key={collection.handle}
+              className="group flex h-full flex-col outline outline-1 outline-black"
+            >
+              {collection.image ? (
+                <Image
+                  src={collection.image.url}
+                  height={320}
+                  width={320}
+                  alt={collection.image.altText}
+                  className="aspect-square w-full object-cover"
                 />
-                <ArrowRight className="transition-all md:-translate-x-2 md:opacity-0 md:group-hover:translate-x-0 md:group-hover:opacity-100" />
+              ) : null}
+              <div className="flex h-full flex-col border-t border-black bg-white p-3">
+                <h2 className="mb-2 font-serif text-lg uppercase md:text-xl">{collection.title}</h2>
+                <div className="mt-auto flex justify-between">
+                  <IndexString
+                    value={collection.products.length}
+                    text="products in
+                  this category"
+                  />
+                  <ArrowRight className="transition-all md:-translate-x-2 md:opacity-0 md:group-hover:translate-x-0 md:group-hover:opacity-100" />
+                </div>
               </div>
-            </div>
-          </Link>
-        ))}
+            </Link>
+          ) : null
+        )}
       </div>
     </section>
   );
