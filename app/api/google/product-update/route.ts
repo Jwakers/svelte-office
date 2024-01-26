e98030078cc70518495f5cd232463da842397000
Jack Wakeham <jackwakeham@gmail.com>
2024-01-26 17:44:05 +0000

    Abstracted requestBody function

--- app/api/google/product-update/route.ts
+++ app/api/google/product-update/route.ts
@@ -2,74 +2,36 @@ import { content_v2_1, google } from 'googleapis';
 import googleAuth from 'lib/google-auth';
 import { verifyWebhook } from 'lib/shopify/verify-webhook';
 import { NextResponse } from 'next/server';
+import { getRequestBody } from '../get-request-body';
 import { Product } from '../types';
+
+export const dynamic = 'force-dynamic'; // Prevents route running during build
 
 export async function POST(req: Request) {
   verifyWebhook(req);
-  const data: Product = await req.clone().json();
 
+  const shopifyProduct: Product = await req.clone().json();
   const auth = googleAuth();
-
+  const googleProductData: content_v2_1.Schema$Product[] = [];
   const content = google.content({
     version: 'v2.1',
     auth
   });
+  const availableForSale = shopifyProduct.variants.some(
+    (variant) => variant.inventory_quantity >= 1
+  );
+  const hasColourVariants = shopifyProduct.options.some(
+    (option) => option.name.toLowerCase() === 'colour'
+  );
 
-  const SITE_URL = `https://${process.env.NEXT_PUBLIC_SITE_URL}`;
+  shopifyProduct.variants.forEach(async (variant, i) => {
+    const requestBody = getRequestBody(shopifyProduct, variant, {
+      isVariant: i >= 1,
+      availableForSale,
+      hasColourVariants
+    });
 
-  const url = `${SITE_URL}/products/${data.handle}`;
-  const availableForSale = data.variants.some((variant) => variant.inventory_quantity >= 1);
-  const hasColourVariants = data.options.some((option) => option.name.toLowerCase() === 'colour');
-  const googleProductData: content_v2_1.Schema$Product[] = [];
-
-  data.variants.forEach(async (variant, i) => {
-    const isVariant = i >= 1;
-    let title = data.title;
-    let availability = availableForSale ? 'in_stock' : 'out_of_stock';
-
-    if (isVariant) {
-      title += ` - ${variant.title}`;
-      availability = variant.inventory_quantity > 0 ? 'in_stock' : 'out_of_stock';
-    }
-
-    const requestBody: content_v2_1.Schema$Product = {
-      offerId: `shopify_GB_${variant.product_id}_${variant.id}`,
-      itemGroupId: isVariant ? `shopify_GB_${variant.product_id}` : undefined,
-      channel: 'online',
-      contentLanguage: 'en',
-      targetCountry: 'GB',
-      title,
-      description: data.body_html,
-      link: url,
-      imageLink: data.images[0] && data.images[0].src,
-      additionalImageLinks: data.images.map((image) => image.src),
-      availability,
-      price: {
-        currency: 'GBP',
-        value: variant?.price
-      },
-      // product specifications are a seperate api endpoint, or convert to graphQL (Will need to use pagination)
-      productLength: undefined,
-      productHeight: undefined,
-      productWidth: undefined,
-      productWeight: undefined,
-      shippingWeight: {
-        unit: variant?.weight_unit,
-        value: variant?.weight
-      },
-      brand: data.vendor,
-      color: hasColourVariants ? variant.title : undefined,
-      googleProductCategory: undefined, // GraphQl - productCategory.productTaxonomyNode.fullName
-      mpn: variant?.barcode,
-      shipping: [
-        {
-          country: 'UK',
-          price: undefined
-        }
-      ]
-    };
-
-    console.log(`Updating product: ${title}`);
+    console.log(`Updating product: ${shopifyProduct.title}`);
 
     const googleProduct = await content.products.insert({
       merchantId: process.env.GOOGLE_MERCHANT_ID,
--- app/api/google/sync-products/route.ts
+++ app/api/google/sync-products/route.ts
@@ -1,17 +1,10 @@
 import { content_v2_1, google } from 'googleapis';
 import googleAuth from 'lib/google-auth';
 import { NextResponse } from 'next/server';
+import { getRequestBody } from '../get-request-body';
 import type { Product } from '../types';
 
 export const dynamic = 'force-dynamic'; // Prevents route running during build
-
-// TODO
-// Abstract auth function
-// Consider converting to use graphQL
-// Create a version that works from webhooks to update single products
-// Make it functional on vercel too
-
-const SITE_URL = `https://${process.env.NEXT_PUBLIC_SITE_URL}`;
 
 export async function GET() {
   try {
@@ -86,7 +79,6 @@ async function updateGoogleMerchantProducts(
   const productData: content_v2_1.Schema$ProductsCustomBatchRequestEntry[] = [];
 
   for (const shopifyProduct of shopifyProducts) {
-    const url = `${SITE_URL}/products/${shopifyProduct.handle}`;
     const availableForSale = shopifyProduct.variants.some(
       (variant) => variant.inventory_quantity >= 1
     );
@@ -95,56 +87,14 @@ async function updateGoogleMerchantProducts(
     );
 
     shopifyProduct.variants.forEach((variant, i) => {
-      const isVariant = i >= 1;
-      let title = shopifyProduct.title;
-      let availability = availableForSale ? 'in_stock' : 'out_of_stock';
-
-      if (isVariant) {
-        title += ` - ${variant.title}`;
-        availability = variant.inventory_quantity > 0 ? 'in_stock' : 'out_of_stock';
-      }
-
-      const requestBody: content_v2_1.Schema$Product = {
-        offerId: `shopify_GB_${variant.product_id}_${variant.id}`,
-        itemGroupId: isVariant ? `shopify_GB_${variant.product_id}` : undefined,
-        channel: 'online',
-        contentLanguage: 'en',
-        targetCountry: 'GB',
-        title,
-        description: shopifyProduct.body_html,
-        link: url,
-        imageLink: shopifyProduct.images[0] && shopifyProduct.images[0].src,
-        additionalImageLinks: shopifyProduct.images.map((image) => image.src),
-        availability,
-        price: {
-          currency: 'GBP',
-          value: variant?.price
-        },
-        // product specifications are a seperate api endpoint, or convert to graphQL (Will need to use pagination)
-        productLength: undefined,
-        productHeight: undefined,
-        productWidth: undefined,
-        productWeight: undefined,
-        shippingWeight: {
-          unit: variant?.weight_unit,
-          value: variant?.weight
-        },
-        brand: shopifyProduct.vendor,
-        color: hasColourVariants ? variant.title : undefined,
-        googleProductCategory: undefined, // GraphQl - productCategory.productTaxonomyNode.fullName
-        mpn: variant?.barcode,
-        shipping: [
-          {
-            country: 'UK',
-            price: undefined
-          }
-        ]
-      };
-
-      const merchantId = process.env.GOOGLE_MERCHANT_ID;
+      const requestBody = getRequestBody(shopifyProduct, variant, {
+        isVariant: i >= 1,
+        availableForSale,
+        hasColourVariants
+      });
 
       productData.push({
-        merchantId,
+        merchantId: process.env.GOOGLE_MERCHANT_ID,
         batchId: productData.length + 1,
         method: 'insert',
         product: requestBody
