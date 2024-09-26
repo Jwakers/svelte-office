295f3ab2e3f1576ebd906c35638d3dd4154ed2c2
Jack Wakeham <jackwakeham82@gmail.com>
2024-09-26 22:00:39 +0100

    feat: add migration file for lavoro desk skus

---
+++ lib/shopify/migrations/12-assign-lavoro-skus.ts
@@ -0,0 +1,78 @@
+import { createAdminRestApiClient } from '@shopify/admin-api-client';
+import * as dotenv from 'dotenv';
+import { ROUTES } from 'lib/constants';
+import { Product } from 'lib/shopify/rest/types';
+dotenv.config({ path: '.env.local' });
+
+// pnpm tsx lib/shopify/migrations/12-assign-lavoro-skus.ts
+
+const client = createAdminRestApiClient({
+  storeDomain: process.env.SHOPIFY_STORE_DOMAIN as string,
+  accessToken: process.env.SHOPIFY_STOCK_MANAGEMENT_ACCESS_TOKEN as string,
+  apiVersion: '2024-04'
+});
+
+const FRAME = {
+  'Black (frame)': 'BADVS/',
+  'Anthracite (frame)': 'AADVS/',
+  'Dark Grey (frame)': 'DGADVS/',
+  'Silver (frame)': 'SADVS/',
+  'Light Grey (frame)': 'LGADVS/',
+  'White (frame)': 'WADVS/',
+  'Raw Steel (frame)': 'RADVS/'
+};
+const TABLETOP = {
+  Black: 'BLA',
+  'Soft Black': 'SBL',
+  Anthracite: 'ANT',
+  Graphite: 'GRA',
+  Stone: 'STO',
+  Cashmere: 'CSH',
+  'Light Grey': 'GRY',
+  White: 'WHI',
+  'Light Concrete': 'LCO',
+  'Dark Concrete': 'DCO',
+  'Ferro Bronze': 'FER',
+  'Anthracite Sherman Oak': 'ASO',
+  'Grey Nebraska Oak': 'GNO',
+  'Brown Oak': 'BOA',
+  'Natural Oak': 'NOA',
+  'Natural Dijon Walnut': 'NDW',
+  Timber: 'TIM',
+  Beech: 'BEE',
+  Maple: 'MAP',
+  'Cascina Pine': 'CAS'
+};
+
+async function migrate() {
+  const productId = '9815731372333';
+  const widthCode = '1200700';
+  const productResponse = await client.get(`${ROUTES.products}/${productId}`);
+  const { product }: { product: Product } = await productResponse.json();
+
+  const variantsToUpdate = product.variants.map((variant) => {
+    const { option1: frameOption, option2: tabletopOption } = variant;
+
+    const sku = `${FRAME[frameOption as keyof typeof FRAME]}${widthCode}${
+      TABLETOP[tabletopOption as keyof typeof TABLETOP]
+    }`;
+
+    return {
+      id: variant.id,
+      product_id: product.id,
+      sku: sku
+    };
+  });
+
+  const response = await client.put(`${ROUTES.products}/${productId}`, {
+    data: {
+      product: {
+        variants: variantsToUpdate
+      }
+    }
+  });
+
+  const responseData = await response.json();
+  console.log(JSON.stringify(responseData, null, 2));
+}
+migrate();
