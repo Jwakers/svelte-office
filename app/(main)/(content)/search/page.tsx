1950ee74b96ee3a2579ea2f07c0718fd5b765f20
Jack Wakeham <jackwakeham82@gmail.com>
2024-10-07 17:23:35 +0100

    reafactor: search for better readability. debounce query for fewer network requests to algolia

--- app/(main)/(content)/search/page.tsx
+++ app/(main)/(content)/search/page.tsx
@@ -1,6 +1,7 @@
 import Results from 'components/layout/search/results';
 import SearchWrapper from 'components/layout/search/search-wrapper';
 import { getAlgoliaIndex } from 'lib/algolia';
+import Head from 'next/head';
 
 export const dynamic = 'force-dynamic';
 
@@ -14,8 +15,18 @@ export const metadata = {
 
 export default async function SearchPage() {
   return (
-    <SearchWrapper>
-      <Results />
-    </SearchWrapper>
+    <>
+      <Head>
+        {/* Preconnect to Algolia to speed up initial user interaction */}
+        <link
+          crossOrigin="anonymous"
+          href={`https://${process.env.NEXT_PUBLIC_ALGOLIA_APP_ID}-dsn.algolia.net`}
+          rel="preconnect"
+        />
+      </Head>
+      <SearchWrapper>
+        <Results />
+      </SearchWrapper>
+    </>
   );
 }
